a3c7ca02b94e3c127602ba54bb658ad39603b9c8
rinodung <tiendungdong@gmail.com>
2018-03-02 14:01:05 +0700

    init

---
+++ WSC2017_TP09_M1_Solution/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSC2017_TP09_M1_Solution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
